ce075045c07cd4e8d72ceec46a7ae0949881dce2
Diego Siena <diegosienafrancisco@hotmail.com>
2014-09-08 20:20:36 -0300

    Versao Final (eu acho)

---
+++ platforms/android/ant-build/ENIC-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Projetos\ENIC\platforms\android\ant-build\ENIC-debug-unaligned.apk : \
+C:\Projetos\ENIC\platforms\android\ant-build\ENIC.ap_ \
+C:\Projetos\ENIC\platforms\android\ant-build\classes.dex \
+C:\Projetos\ENIC\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ platforms/android/assets/www/js/sobreoapp.js
@@ -0,0 +1,29 @@
+var app = {
+// Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('backbutton', voltar, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+    }
+};
+
+function voltar() {
+    navigator.app.backHistory();
+}
